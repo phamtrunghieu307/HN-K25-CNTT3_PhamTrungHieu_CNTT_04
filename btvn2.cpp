9a2cf8089e3472422fc2687180bb524aa624adcf
phamtrunghieu307 <trunghieupham843@gmail.com>
2025-10-27 21:44:43 +0700

    ss4 ex.2

---
+++ btvn2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+ int n;
+ printf("nhap so nguyen ");
+ scanf("%d",&n);
+ if( n % 2 ==  0) { printf("so vua nhap la so chan"); 
+ } else{ printf("so vua nhap la so le"); 
+ } 
+return 0;
+}
+
