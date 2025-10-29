d877fe85c2a76ca7c4c06afe550fa6bce0c47164
phamtrunghieu307 <trunghieupham843@gmail.com>
2025-10-29 22:56:09 +0700

    ss6 ex.4

---
+++ btvn4 (2).cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int so; 
+printf("moi ban nhap so ");
+scanf("%d",&so);
+while(so < 1 || so > 10){
+	printf("loi, nhap lai ");
+	scanf("%d",&so); 
+} 
+int i; 
+for(i=1;i<=10;i++){
+	printf("%d * %d = %d\n",so,i,so*i); 
+} 
+return 0;
+}
+
