9c884c227e95b52b93034e607c9e8d5415c6b94e
phamtrunghieu307 <trunghieupham843@gmail.com>
2025-11-06 09:42:40 +0700

    nop bai hackathon

---
+++ HN-K25-CNTT3_PhamTrungHieu_04/hackathon.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int a[MAX],n=0,chon x, vt;
+	
+	do{
+	printf("\n-------MENU-------\n");
+	printf("1.nhap so luong phan tu va gia tri tung phan tu cho mang\n");
+	printf("2.in cac so lon hon gia tri trung binh cua cac pha tu co trong mang\n");
+	printf("3.in ra gia tri nho nhat cua cac phan tu trong mang\n");
+	printf("4.them moi mot phan tu vao dau mang\n");
+	printf("5.xoa phan tu tai mot vi tri cu the(nguoi dung nhap)\n");
+	printf("6.sap xep mang theo thu tu tang dan(Selection sort)\n");
+	printf("7.kiem tra su ton tai cua phan tu trong mang(phan tu tim kiem do nguoi dung nhap vao, su dung thuat toan Binary Search)\n");
+	printf("8.in ra cac phan tu xuat hien dung 2 lan trong mang\n");
+	printf("9.in so lan xuat hien cua 1 phan tu(phan tu do nguoi dung nhap vao)\n");
+	printf("0.thoat\n");
+	printf("-------------------/n");
+	printf("chon:");
+	scanf("%d,&chon");
+	
+	switch chon {
+	
+	}
+    }
+}
